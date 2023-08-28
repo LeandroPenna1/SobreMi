f6cce7640b46cc3e7191636bf72d57d53794984c
Leandro Penna <134015680+LeandroPenna1@users.noreply.github.com>
2023-08-28 08:19:14 -0300

    Agrego parte 3 y 4 del tp2

--- tp2/piedraPapelTijeras.js
+++ tp2/piedraPapelTijeras.js
@@ -1,5 +1,6 @@
 /////////////////////////////////////////  OBTENER NOMBRE USUARIO  ///////////////////////////////////////
 function miFuncion() {
+    //Se utiliza javascript para obtener el nombre de usuario//
     var nombreUsuario = document.getElementById("nombre").value;
     if (nombreUsuario !== "") {
         //de esta manera, verificamos que el campo no esté vacío//
@@ -9,90 +10,31 @@ function miFuncion() {
     }
 }
 
+/////////////////////////////// SELECCION DE OPCIONES Y GANADOR //////////////////////////////////////////
 
-/////////////////////////////////////////  JUGADA COMPUTADORA  ///////////////////////////////////////
-let jugada = "";
-var y = 0;
-var x = 3;
-var random = Math.floor(Math.random() * (+y - +x)) + +x;
-    
-function obtenerJugadaComputadora() {
-switch(random) {
-    case 0:
-        jugada = "piedra";
-    break;
-    case 1:
-        jugada = "papel";
-    break;
-    case 2:
-        jugada = "tijera";
-    break;
-    default:
-        error;
-    }
-console.log("La computadora eligió " + jugada);
+const opciones = ["piedra", "papel", "tijera"];
+
+function jugar(opcionJugador) {
+  const opcionComputadora = opciones[Math.floor(Math.random() * 3)];
+  let resultado = "";
+//Se comparan las opciones para determinar el ganador//
+  if (opcionJugador === opcionComputadora) {
+    resultado = "Empate";
+  } else if (
+    (opcionJugador === "piedra" && opcionComputadora === "tijera") ||
+    (opcionJugador === "papel" && opcionComputadora === "piedra") ||
+    (opcionJugador === "tijera" && opcionComputadora === "papel")
+  ) {
+    resultado = "¡Ganaste!";
+  } else {
+    resultado = "¡Perdiste!";
+  }
+
+  document.getElementById("resultado").textContent = `Resultado: ${resultado}`;
 }
-    
-///////////////////////////////////////////  JUGADA USUARIO //////////////////////////////////////////
-
-    /* const readlineSync = require('readline-sync');
-    let eleccion = readlineSync.question("Por favor, envia tu eleccion ('Piedra', 'Papel' o 'Tijera'): \n"); */
-
-const eleccion = document.getElementById("eleccion");
-const eleccionReflejada = documen.getElementById("eleccionReflejada");
-eleccion.addEventListener("input", function(event) {
-    eleccionReflejada.textContent = event.target.value;
-});
-
-let eleccionMinuscula = eleccion.toLowerCase();
-function obtenerJugadaUsuario() {
-    var eleccionUsuario = document.getElementById("eleccion").value;
-switch(eleccionMinuscula) {
-    case "piedra":
-    break;
-    case "papel":
-    break;
-    case "tijera":
-    break;
-    default:
-        console.log("Vuelve a intentarlo. Recuerda que las opciones son: 'Piedra', 'Papel' o 'Tijera'.");
-        process.exit(1);
-    }
-console.log ("El usuario eligió " + eleccionMinuscula);
-    
-}
-   
-///////////////////////////////////////  DETERMINAR GANADOR  /////////////////////////////////////
-    function determinarGanador(usuar, compu) {
-        if(usuar === compu){
-            console.log("El resultado fue: EMPATE.");
-        } else if (usuar === "papel" && compu === "piedra"){
-            console.log("El resultado fue: GANA EL USUARIO.");
-        } else if (usuar === "tijera" && compu === "piedra"){
-            console.log("El resultado fue: GANA LA COMPUTADORA.");
-        } else if (usuar === "piedra" && compu === "papel"){
-            console.log("El resultado fue: GANA LA COMPUTADORA.");
-        } else if (usuar === "tijera" && compu === "papel"){
-            console.log("El resultado fue: GANA EL USUARIO.");
-        } else if (usuar === "piedra" && compu === "tijera"){
-            console.log("El resultado fue: GANA EL USUARIO.");
-        } else {
-            //usuar === "papel" && compu === "tijera"
-            console.log("El resultado fue: GANA LA COMPUTADORA.");
-        }
-            
-    }
-
-
-    obtenerJugadaUsuario()
-    obtenerJugadaComputadora()
-    determinarGanador(eleccionMinuscula, jugada)
-
-////////////////////////////////////////    EXTRA 1     ////////////////////////////////////////////
-
-const piedra = 0;
-const papel = 1;
-const tijera = 2;
-
-////////////////////////////////////////    EXTRA 2     ////////////////////////////////////////////
-
+/* Aquí, se detecta con un Event Listener qué opción clickeó (y eligió) el jugador.
+Para ello, cada imagen tenía su respectivo id, por ende se busca por ID qué es lo que eligió.
+ */
+document.getElementById("piedra").addEventListener("click", () => jugar("piedra"));
+document.getElementById("papel").addEventListener("click", () => jugar("papel"));
+document.getElementById("tijera").addEventListener("click", () => jugar("tijera"));
